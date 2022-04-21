8dbe25941ba9c859ce243abcb356fd745277da90
lodashreload <icarusjapan@gmail.com>
2022-04-20 20:37:11 -0400

    Update react router dom added home + unfinshed html

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
 import React from 'react'
+import { Route, Routes } from 'react-router'
+import {Home} from './pages/home'
 
 export function App() {
-  return <div>Hello, World </div>
+  return <div>
+    <header>
+      <h1>What occupies my daydreams</h1>
+      <h2>Photo Gallery of Hobbies</h2>
+    </header>
+    <main>
+      <Routes>
+        <Route path="/" element={<Home />}>
+        </Route>
+        
+      </Routes>
+   </main>
+  </div>
 }
---
+++ src/pages/Home.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+export function Home() {
+  return (
+    <div>
+      <main>
+        <button>Home</button>
+        <section>
+          <h2 className="Pic-1">
+            <p></p>
+          </h2>
+        </section>
+      </main>
+    </div>
+  )
+}

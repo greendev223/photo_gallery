9e77a28d01e0cffca8cbc264f0217908ca291802
lodashreload <icarusjapan@gmail.com>
2022-04-21 00:13:27 -0400

    updated adventure mode json

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+// 4QVH2LI6YTXDN0J9fpaoHHtDeT08d56JWC_XYlLLvKo
+// 6PCaQLL55oRIjVLolrFMyx4FT5cDFiyrbAlMLtFvl2M
 import React from 'react'
 import { Route, Routes } from 'react-router'
 import {Home} from './pages/home'

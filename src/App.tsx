3d2848aadfc09092efa3393cc9062f502df5dbe8
lodashreload <icarusjapan@gmail.com>
2022-04-21 19:39:45 -0400

    All the changes.  images mostly work

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,23 @@
 // 6PCaQLL55oRIjVLolrFMyx4FT5cDFiyrbAlMLtFvl2M
 import React from 'react'
 import { Route, Routes } from 'react-router'
-import {Home} from './pages/Home'
+import { Home } from './pages/Home'
 
 export function App() {
-  return <div>
-    <header>
-      <h1>What occupies my daydreams</h1>
-      <h2>Photo Gallery of Hobbies</h2>
-    </header>
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />}>
-        </Route>
-        <Route path="/home" element={<Home />}>
-        </Route>
-        
-      </Routes>
-   </main>
-  </div>
+  return (
+    <div>
+      <header>
+        <h1>What occupies my daydreams</h1>
+        <h2>Photo Gallery of Hobbies</h2>
+      </header>
+      <main>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="/home" element={<Home />}>
+            HOME
+          </Route>
+        </Routes>
+      </main>
+    </div>
+  )
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,25 +4,32 @@ import data from './data2.json'
 
 //This the main landing page for the phot gallery.
 //It is the first page that is loaded when the app is started.
-//`/${Object.keys(data)[0]}`I need this explained in class. 
+//`/${Object.keys(data)[0]}` Same as /Nippon (write up more later) I need this explained in class. 
 export function Home() {
-  return (<>
-    <div>
-      <main>
-        <button>Home</button>
-        <section>
-          <div>
-          <h2 className="Pic-1">
-            <Link to={`/${Object.keys(data)[0]}`}></Link>
-          </h2>
-            <p>
-              {data.Nippon.description}
-            </p>
-            <img className="pic-1"src={data.Nippon.photos[0].imageURL} alt={data.Nippon.title} />
-          </div>
-        </section>
-      </main>
-    </div>
- </> )
+  console.log(Object.keys(data)[0])
+  return (
+    <>
+      <div>
+        <main>
+          {/* <button>
+           Home
+          </button> */}
+          <section>
+            <div>
+              <h2 className="Pic-1">
+                <Link to={`/${Object.keys(data)[0]}`}>Nippon</Link>
+              </h2>
+              <p>{data.Nippon.description}</p>
+              <img
+                className="pic-1"
+                src={data.Nippon.photos[0].imageURL}
+                alt={data.Nippon.photos[0].title}
+              />
+            </div>
+          </section>
+        </main>
+      </div>
+    </>
+  )
   
 }
